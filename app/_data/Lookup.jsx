63c7a334e8c6895134183aa54bdd73fb0b269fce
TapamitaMondal07 <tapomita.tae007@gmail.com>
2025-02-09 22:47:01 +0530

    Create some  UI components

---
+++ app/_data/Lookup.jsx
@@ -0,0 +1,16 @@
+export default{
+  HeroHeading:'AI Logo Maker',
+  HeroSubheading:'Perfect Logos for Apps, Businesses and Websites',
+  HeroDesc:'Craft unique and professional logos effortlessly with out AI-powered tool. Perfect for apps, businesses and more !',
+  InputTitlePlaceholder: 'Enter your logo name',
+  LogoTitle:'Logo Title',
+  LogoTitleDesc:'Add your business, app or website name for a customized logo',
+  LogoDescTitle:'Describe Your Logo Vision',
+  LogoDescDesc:'Share your ideas, themes or inspirations to create',
+  LogoIdeaTitle:'Select your design idea',
+  LogoIdeaDesc:'Choose a design style that aligns with your vision',
+  LogoDesignTitle:'Choose your logo style',
+  LogoDesignDesc:'Select the type of logo design that best represents',
+  LogoColorPaletteTitle:'Choose your color palette',
+  LogoColorPaletteDesc:'Pick the olors that reflect your brands'
+}
